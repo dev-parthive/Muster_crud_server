d93a998c70ac091d8ade0c3273fe23c4d7614d35
dev-parthive <dev-parthive>
2022-11-17 12:33:10 +0600

    prduct add  remove update means crud operation done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('colors')
 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express() ;
 
@@ -46,7 +46,8 @@ const productCollection =  client.db("crud-operation").collection("products")
 const usersCollection = client.db("crud-operation").collection("users") 
 
 
-//endpoint 
+//endpoint
+// create producs  
 app.post('/product', async(req, res)=>{
  // res.send(getAllProduts())
 try {
@@ -74,6 +75,105 @@ catch(error){
 }
 });
 
+// read/ get products from DB
+app.get('/product', async (req, res) =>{
+    try{
+        const cursor =   productCollection.find({})
+       const products = await  cursor.toArray()
+       res.send({
+        success: true, 
+        message: 'got the data', 
+        data: products 
+       })
+    }
+    catch(error){
+        console.log(error.name.bgRed, error.message.bold)
+        
+        res.send({
+            succeess: false, 
+            message: "something went wrong "
+        })
+
+    }
+})
+
+//Prodcut delete api 
+app.delete('/product/:id', async (req, res)=>{
+    const id = req.params.id
+
+    try{
+        // const product = await productCollection.findOne({_id: ObjectId(id)})
+        // if(product?._id){
+        //     res.send({
+        //         success: false, 
+        //         erro: "Product doesn't exist "
+        //     });
+        //     return;
+        //  } 
+
+        const result = await productCollection.deleteOne({_id: ObjectId(id)})
+        if(result.deletedCount){
+            console.log("successfully deleted".yellow)
+            res.send({
+                success: true,
+                message: `product is deleted`
+            })
+        }else{
+            console.log("something went wrong ".red)
+        }
+    }
+    catch(error){
+        res.send({
+            success: false,
+            error: error.message
+        })
+    }
+}) 
+
+/// to get the specific product 
+app.get('/product/:id',  async(req, res)=>{
+    try {
+        const  id = req.params.id
+        const product = await productCollection.findOne({_id: ObjectId(id)})
+        res.send({
+            success: true ,
+            data: product
+        })
+        
+    }
+    catch(error){
+        res.send({
+            success : false,
+            error: error.message
+        })
+
+    }
+}) 
+
+//akn kaj holo edit korar data k db te patano 
+app.patch("/product/:id", async(req, res)=>{
+    const {id} = req.params
+    try{
+        const result = await productCollection.updateOne({_id: ObjectId(id)} , {$set: req.body})
+        if(result.modifiedCount){
+            res.send({
+                success: true, 
+                message: 'successfully updated '
+            })
+        }else{
+            res.send({
+                success: false, 
+                error: "Couldn't update the product"
+            })
+        }
+    }
+    catch(error){
+        res.send({
+            succeess: false,
+            error: error.message
+        })
+    }
+})
 
 app.get('/', (req, res)=>{
     res.send('server is running...')
